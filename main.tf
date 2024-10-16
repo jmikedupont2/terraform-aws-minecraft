6cda6eaeb0cc8119e8656c6ecb5ef9887cf95f6e
Jose Jorge Díaz <jj.diaz002@gmail.com>
2024-10-16 15:07:23 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ locals {
 
 // Keep labels, tags consistent
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=master"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.25.0"
 
   namespace   = var.namespace
   stage       = var.environment
